0227ca5486ad95a8d96c274fd12a7a998d60ab6f
Io <stubrown357@gmail.com>
2023-01-29 21:37:45 -0600

    init tree stuff for challenge

--- ruby/index.rb
+++ ruby/index.rb
@@ -8,5 +8,27 @@ puts "now print 4 at a time, 4 times"
 # do the same but with each_slice in Enumerable
 
 # make a program that builds a tree from a specified hash
+class Tree
+  attr_accessor :children, :node_name
+
+  def initialize(tree_hash={})
+    @children = get_children(tree_hash)
+    @node_name = get_name(tree_hash)
+  end
+
+  def get_children(tree_hash)
+    
+  end
+
+  def visit_all(&block)
+    visit &block
+    children.each {|c| c.visit_all &block}
+  end
+
+  def visit(&block)
+    block.call self
+  end
+end
+
 
 # write a grep that will read contents of a file and print any phrase that is matched by a simple regex
