47b299b2bf1f1f5de8cb12a51714d6057ad42a4d
Io <stubrown357@gmail.com>
2023-01-31 21:44:42 -0600

    init file access

--- ruby/day2.rb
+++ ruby/day2.rb
@@ -8,28 +8,33 @@ puts "now print 4 at a time, 4 times"
 # do the same but with each_slice in Enumerable
 
 # make a program that builds a tree from a specified hash
-class Tree
-  attr_accessor :children, :node_name
-
-  def initialize(tree_hash={})
-    @children = get_children(tree_hash)
-    @node_name = get_name(tree_hash)
-  end
-
-  def get_children(tree_hash)
-    # map through hash and get node name and children.... this should probably just be done in initialize
-    # tree_hash.map {|k,v| [k, v.get_children] }
-  end
-
-  def visit_all(&block)
-    visit &block
-    children.each {|c| c.visit_all &block}
-  end
-
-  def visit(&block)
-    block.call self
-  end
-end
+#class Tree
+#  attr_accessor :children, :node_name
+#
+#  def initialize(tree_hash={})
+#    @children = get_children(tree_hash)
+#    @node_name = get_name(tree_hash)
+#  end
+#
+#  def get_children(tree_hash)
+#    # map through hash and get node name and children.... this should probably just be done in initialize
+#    # tree_hash.map {|k,v| [k, v.get_children] }
+#  end
+#
+#  def visit_all(&block)
+#    visit &block
+#    children.each {|c| c.visit_all &block}
+#  end
+#
+#  def visit(&block)
+#    block.call self
+#  end
+#end
 
 
 # write a grep that will read contents of a file and print any phrase that is matched by a simple regex
+puts "please enter the path to the file:"
+file_path = gets.chomp
+puts "please enter the word you want to find:"
+word = gets.chomp
+file = File.fnmatch(word)
