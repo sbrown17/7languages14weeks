a9d64cf68d4a76d887c07314600fcc1083a8d6f4
Io <stubrown357@gmail.com>
2023-02-04 08:39:07 -0600

    init day 3 ruby practice

---
+++ ruby/day3.rb
@@ -0,0 +1,49 @@
+# modify the csv application to support an "each" method to return a CsvRow object
+# use method_missing on that csv row to return the value for the column for a given heading
+module ActsAsCsv
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    def acts_as_csv
+      include InstanceMethods
+    end
+  end
+
+  module InstanceMethods
+    def read
+      @csv_contents = []
+      filename = self.class.to_s.downcase + '.txt'
+      file = File.new(filename)
+      @headers = file.gets.chomp.split(', ')
+
+      file.each do |row|
+        @csv_contents << row.chomp.split(', ')
+      end
+    end
+
+    attr_accessor :headers, :csv_contents
+    def initialize
+      read
+    end
+  end
+end
+
+class RubyCsv
+  include ActsAsCsv
+  acts_as_csv
+end
+
+m = RubyCsv.new
+puts m.headers.inspect
+puts m.csv_contents.inspect
+
+
+
+# one, two
+# lions, tigers
+# allow an api that works like this:
+# csv = RubyCsv.new
+# csv.each {|row| puts row.one}
+# should print "lions"
